b1f02e1057c5e747dd1a8d36776e31bb0940c930
Stephen Armbrecht <sarmbrec@gmail.com>
2019-10-01 14:46:35 -0400

    clean up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,59 +1,38 @@
-
-
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-//import { FormsModule } from '@angular/forms';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
-//import { BrowserModule } from '@angular/platform-browser';
-//import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
 import {ReactiveFormsModule } from '@angular/forms';
+//import { FormsModule } from '@angular/forms';
 
-//import { AppRoutingModule } from './app-routing.module';
-//import { AppComponent } from './app.component';
+import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
-
 import { CarouselHomePageComponent } from './carousel-home-page/carousel-home-page.component';
 import { GitHubIconLinkComponent } from './git-hub-icon-link/git-hub-icon-link.component';
-// import { AngularComponent } from './angular/angular.component';
 import { AngularjsComponent } from './angularjs/angularjs.component';
-// import { Html5Component } from './html5/html5.component';
-// import { SassComponent } from './sass/sass.component';
-// import { JavascriptComponent } from './javascript/javascript.component';
 import { KnowledgeBaseComponent } from './knowledge-base/knowledge-base.component';
 import { FooterComponent } from './footer/footer.component';
 import { KnowledgeBaseDetailComponent } from './knowledge-base-detail/knowledge-base-detail.component';
 import { KbDetailComponent } from './knowledge-base/kb-detail/kb-detail.component';
 
-
-
 // The App module file is basically where all imports and generated files are recorded 
 // with their configuration metadata in the NgModule object. 
 // As a footnote, all components go into declarations, services into providers and 
 // modules into imports.
-
-
 @NgModule({
   declarations: [
-    NavComponent,
     AppComponent,
+    NavComponent,
     AboutComponent,
     ContactComponent,
     HomeComponent,
     CarouselHomePageComponent,
     GitHubIconLinkComponent,
-    //AngularComponent,
     AngularjsComponent,
-    //Html5Component,
-    //SassComponent,
-    //JavascriptComponent,
     KnowledgeBaseComponent,
     FooterComponent,
     KnowledgeBaseDetailComponent,
