471d5af5182d7d13388db7ea73a2db52b1080fc6
Stephen Armbrecht <sarmbrec@gmail.com>
2019-10-03 20:24:38 -0400

    asynch

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +11,14 @@ export class HeroService {
 
   constructor() { }
 
-  getHeroes(): Hero[] {
-    return HEROES;
-  }
+  // synchrounous
+  // getHeroes(): Hero[] {
+  //   return HEROES;
+  // }
   
+  // asynchronous with observable
+  getHeroes(): Observable<Hero[]> {
+  return of(HEROES);
+}
+
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HEROES } from '../mock-heroes';
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,17 +11,30 @@ import { Hero } from '../hero';
 
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes = [];
   selectedHero: Hero;
    
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
+  // synchronous operation
+  //getHeroes(): void {
+    //This will not work in a real app. You're getting away with it now because the service currently returns mock heroes. 
+    //But soon the app will fetch heroes from a remote server, which is an inherently asynchronous operation.
+    //this.heroes = this.heroService.getHeroes();
+  //}
+
+  // asynchronous operation
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 
 }
