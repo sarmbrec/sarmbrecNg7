6aa15c5dce624b180ce407e893c8fd4cf96b27bd
Stephen Armbrecht <sarmbrec@gmail.com>
2019-10-13 15:24:08 -0400

    contact

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { AppComponent } from '../app.component';
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -11,6 +13,10 @@ export class ContactComponent implements OnInit {
   contactForm: FormGroup;
   submitted = false;
   success = false;
+
+  appTitle = new AppComponent().title;
+
+
 
   constructor(private formBuilder: FormBuilder) {
     this.contactForm = this.formBuilder.group({
