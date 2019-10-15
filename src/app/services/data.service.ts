b01f3ad75d000ce0031b343bfb1d1351cbe688ad
Stephen Armbrecht <sarmbrec@gmail.com>
2019-10-15 12:06:07 -0400

    latest

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -13,7 +13,6 @@ import { HttpHeaders } from '@angular/common/http';
 const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type':  'application/json'
-        //'Authorization': 'my-auth-token'
       })
     };
 
