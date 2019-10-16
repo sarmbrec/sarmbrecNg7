9c5a0d3c00561f920b64989f1f7ef2257d04d9c3
Stephen Armbrecht <sarmbrec@gmail.com>
2019-10-16 14:13:12 -0400

    experience chart work

--- src/app/experience/experience-detail/experience-detail.component.ts
+++ src/app/experience/experience-detail/experience-detail.component.ts
@@ -18,73 +18,13 @@ import { Color, BaseChartDirective } from 'ng2-charts';
 export class ExperienceDetailComponent implements OnInit {
 
   detail$: Observable<ExperienceInterface>;
-
-  barChartOptions: ChartOptions = {
-    title: {
-      display: true,
-      text: '% used',
-      position: 'bottom'
-    },
-    responsive: true,
-    // legend: {
-    //   display: true,
-    //   labels: {
-    //       fontColor: 'rgb(255, 99, 132)'
-    //   }
-    // },
-    scales: {
-      xAxes: [{
-        display: true,
-        gridLines: {
-          display: true
-        },
-        ticks: {
-          display: true,
-          beginAtZero: true
-        }
-      }],
-      yAxes: [{
-          display: true,
-          position: 'right',
-        gridLines: {
-          display: true
-        },
-        ticks: {
-          display: true,
-          beginAtZero: true
-        }
-      }]
-    }
-    // ,
-    // plugins: {
-    //   datalabels: {
-    //     anchor: 'end',
-    //     align: 'end',
-    //   }
-    // }
-  };
-  barChartType: ChartType = 'horizontalBar';
+  barChartOptions: ChartOptions = {};
+  barChartLabels: Label[] = [];
+  barChartData: ChartDataSets[];
+  chartColors: Color[] = [];
+  chartTechData = [];
+  barChartType: ChartType = 'bar'; // 'horizontalBar';
   barChartLegend = false;
-  barChartLabels: Label[] = ['HTML 5', 'JavaScript', 'jQuery', 'SASS', 'ASP.Net'];
-  barChartData: ChartDataSets[] = [
-    {
-      data: [65, 59, 80, 81, 56, 55, 40]
-    }
-  ];
-  chartColors: Color[] = [
-    {
-      borderColor: 'rgba(0, 123, 255, 1)',
-      borderWidth: 1,
-      backgroundColor: 'rgba(0, 123, 255, 0.3)'
-    },
-  ];
-
-  // public barChartLabels: Label[] = ['HTML', 'SASS', 'JavaScript', 'AngularJS', 'Java'];
-  // public barChartData: SingleDataSet = [95, 70, 80, 60, 20];
-  // public barLegend = true;
-  // public barChartLegend = false;
-  // public barChartType: ChartType = 'bar';
-
 
   @ViewChildren('developedFor') items: QueryList<any>;
 
@@ -96,10 +36,86 @@ export class ExperienceDetailComponent implements OnInit {
 
   ngOnInit(): void {
 
-     this.detail$ = this.route.paramMap.pipe(
-      switchMap((params: ParamMap) =>
-        this.service.getExperienceDetailAsync(params.get('id')))
-      );
+    this.detail$ = this.route.paramMap.pipe(
+      switchMap((params: ParamMap) => this.service.getExperienceDetailAsync(params.get('id')))
+    );
+
+    this.detail$.subscribe(response => {
+
+      // console.log('response  = ', response);
+
+      for (const item of response.technologies) {
+        this.barChartLabels.push(item.title);
+        this.chartTechData.push(item.amountUsed);
+        // console.log(item);
+        // console.log(this.barChartLabels);
+        // console.log(this.chartTechData);
+      }
+
+
+      this.barChartOptions = {
+        title: {
+          display: false,
+          //text: '% used',
+          position: 'bottom'
+        },
+        layout: {
+          padding: {
+              left: 0,
+              right: 0,
+              top: 0,
+              bottom: 0
+          }
+      },
+        responsive: true,
+        scales: {
+          xAxes: [{
+            display: true,
+            gridLines: {
+              display: true
+            },
+            ticks: {
+              display: true,
+              beginAtZero: true
+            }
+          }],
+          yAxes: [{
+            display: true,
+            position: 'right',
+            gridLines: {
+              display: true
+            },
+            ticks: {
+              display: true,
+              fontSize: 8,
+              beginAtZero: true
+            }
+          }]
+        }
+        // ,
+        // plugins: {
+        //   datalabels: {
+        //     anchor: 'end',
+        //     align: 'end',
+        //   }
+        // }
+      };
+      //this.barChartLabels = ['HTML 5', 'JavaScript', 'jQuery', 'SASS', 'ASP.Net'];
+      this.barChartData = [
+        {
+          data: this.chartTechData
+        }
+      ];
+      this.chartColors = [
+        {
+          borderColor: 'rgba(0, 123, 255, 1)',
+          borderWidth: 1,
+          backgroundColor: 'rgba(0, 123, 255, 0.3)'
+        },
+      ];
+
+    });
+
 
   }
 
--- src/app/interfaces/experience.ts
+++ src/app/interfaces/experience.ts
@@ -6,4 +6,5 @@ export interface ExperienceInterface {
   logo: string;
   dates: object;
   achievements: object;
+  technologies: any;
 }
