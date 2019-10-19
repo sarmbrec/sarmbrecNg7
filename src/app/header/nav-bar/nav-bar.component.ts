cd54ca55cc9a0d7ca8fb0b97c3a64ac6b0db2759
Stephen Armbrecht <sarmbrec@gmail.com>
2019-10-19 11:45:41 -0400

    design tweaks and ux tweaks

--- src/app/header/nav-bar/nav-bar.component.ts
+++ src/app/header/nav-bar/nav-bar.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AppComponent } from '../../app.component';
+import { Observable } from 'rxjs';
+import { ExperienceMenuInterface } from 'src/app/interfaces/experience-menu';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -9,10 +12,14 @@ import { AppComponent } from '../../app.component';
 export class NavBarComponent implements OnInit {
   collapsed = true;
   appTitle = new AppComponent().title;
+  experiencesMenu$: Observable<ExperienceMenuInterface[]>;
 
-  constructor() { }
+  constructor(private service: DataService) { }
 
   ngOnInit() {
+
+    this.experiencesMenu$ = this.service.getExperiencesMenuAsync();
+
   }
 
 }
