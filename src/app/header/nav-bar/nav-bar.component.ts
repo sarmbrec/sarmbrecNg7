b832a0198e18c0a1ef584356671e28278db4ad91
Stephen Armbrecht <sarmbrec@gmail.com>
2019-10-20 13:33:43 -0400

    build over budget

--- src/app/header/nav-bar/nav-bar.component.ts
+++ src/app/header/nav-bar/nav-bar.component.ts
@@ -6,12 +6,12 @@ import { DataService } from 'src/app/services/data.service';
 import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 import {filter} from 'rxjs/operators';
 
-
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
+
 export class NavBarComponent implements OnInit {
   collapsed = true;
   appTitle = new AppComponent().title;
@@ -25,21 +25,10 @@ export class NavBarComponent implements OnInit {
 
     this.experiencesMenu$ = this.service.getExperiencesMenuAsync();
 
-    // this.router.events.subscribe( (url: any) => console.log(url) );
-    // console.log('this.router.url = ', this.router.url);  // to print only path eg:"/login"
-    // console.log('this.activatedRoute.snapshot.url[0].path = ', this.activatedRoute.snapshot.url[0].path);
-    // console.log(this.route.url); //  /routename
-
-
     this.router.events.pipe(
       filter((event: any) => event instanceof NavigationEnd)
     ).subscribe(event => {
-          this.urlPath = event.url;
-
-          console.log('this.urlPath = ', this.urlPath);
-          this.routeHasExperience = this.urlPath.indexOf('/experience') > -1;
-
-          console.log('this.routeHasExperience = ', this.routeHasExperience);
+          this.routeHasExperience = event.url.indexOf('/experience') > -1;
       });
     }
 
