408502ba0d93b2bc6a3e3296d787bf634c02565b
Stephen Armbrecht <sarmbrec@gmail.com>
2019-10-23 10:57:37 -0400

    build

--- src/app/header/nav-bar/nav-bar.component.ts
+++ src/app/header/nav-bar/nav-bar.component.ts
@@ -6,10 +6,10 @@ import { DataService } from 'src/app/services/data.service';
 import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 import {filter} from 'rxjs/operators';
 
-import { NavbarComponent as navbar } from '../../../node_modules/angular-bootstrap-md/navbars/navbar.component';
+//import { NavbarComponent as navbar } from '../../../node_modules/angular-bootstrap-md/navbars/navbar.component';
 
 
-@ViewChild('navbarMain', {static : true})
+//@ViewChild('navbarMain', {static : true})
 
 @Component({
   selector: 'app-nav-bar',
